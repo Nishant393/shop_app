cdee2e4fe82ca56e90c8ec462e2d973a4e218804
Nishant393 <nishantpawar393@gmail.com>
2025-01-03 16:51:26 +0530

    created All rotues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,40 @@
-import './App.css'
+import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./Auth/Auth_Pages/SignUp"
+import SignIn from "./Auth/Auth_Pages/SingIn"
+import Auth from "./Auth/Auth"
+import Home from "./Root/Pages/Home"
+import Contact from "./Root/Pages/Contact"
+import Kart from "./Root/Pages/Kart"
+import Like from "./Root/Pages/Like"
+import Products from "./Root/Pages/Products"
+import Orders from "./Root/Pages/Orders"
+import Root from "./Root/Root"
+import NotFound from "./Root/Pages/NotFound";
 
-function App() {
+const App = () => {
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <>
+      <main className="h-screen flex">
+          <Routes>
+            <Route element={<Root />}>
+              <Route index element={<Home />} />
+              <Route path="/contact" element={<Contact/>}/>
+              <Route path="/kart" element={<Kart />}/>
+              <Route path="/like" element={<Like />}/>
+              <Route path="/products" element={<Products />}/>
+              <Route path="/orders" element={<Orders />}/>
+            </Route>
+            <Route element={<Auth />}>
+              <Route element={<SignIn />} path="/sign-in" />
+              <Route element={<SignUp />} path="/sign-up" />
+            </Route>
+            <Route path="*" element={<NotFound/>} /> 
+          </Routes>
+      </main>
+    </>
+  );
+};
 
-export default App
+export default App;
