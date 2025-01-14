38cecd747af09bb9201faf0d46920578a8dda9ac
Nishant393 <nishantpawar393@gmail.com>
2025-01-14 11:40:33 +0530

    edit navbar

--- client/src/Auth/Auth_Pages/SingIn.jsx
+++ client/src/Auth/Auth_Pages/SingIn.jsx
@@ -1,50 +1,70 @@
 import { useState } from 'react'
 import Button from '@mui/joy/Button';
-import { useInputValidation, useStrongPassword } from "6pp";
+import { useInputValidation } from "6pp";
 import Input from '@mui/joy/Input';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import toast from "react-hot-toast";
 
 
 
 const SignIn = () => {
 
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate()
 
 
   const name = useInputValidation("");
   const password = useInputValidation("");
-  const email = ""
-  const mobileNumber = ""
-  const handleSubmit = (e) => {
+  const email = useInputValidation("")
+  const mobileNumber = useInputValidation("")
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("jjrk")
+    setIsLoading(true)
     try {
-      setIsLoading(true)
-      console.log(data)
-      setIsLoading(false)
+      await axios.post("http://localhost:3000/user/login", {
+        email: email.value,
+        password: password.value,
+      }).then((e) => {
+        if (e.data.success) {
+          navigate("/")
+        }
+        setIsLoading(false)
+        console.log(e.data.success)
+        console.log(e.data.message)
+        toast.success(e.data.message);
+      }).catch((e) => {
+        toast.error(e.response.data.error.message);
+        setIsLoading(false)
+        console.log(e.response.data.error.message)
+        console.log(e)
+        console.log(e.response.data.success)
+      })
     } catch (error) {
       console.log(error)
     }
   };
 
-
-
-
   return (
-    <div className='m-auto' >
+    <div className='m-auto w-4/6' >
       <form
         style={{
           width: "100%",
           marginTop: "1rem",
         }}
+        className='flex justify-center'
         onSubmit={handleSubmit}
       >
-        <div className='flex gap-10 flex-col'>
+        <div className='flex gap-5 justify-center w-1/2 flex-col'>
+          <h1 className='mx-auto' >Login</h1>
+          
           <Input
             fullWidth
             placeholder="email"
             required
-            value={email}
+            value={email.value}
             onChange={email.changeHandler}
             sx={{
               "--Input-radius": "11px",
@@ -53,6 +73,7 @@ const SignIn = () => {
               "--Input-paddingInline": "15px"
             }}
           />
+          
           <Input
             fullWidth
             type="password"
@@ -68,22 +89,21 @@ const SignIn = () => {
             }}
           />
 
-          
-            <Button
-              variant="contained"
-              color="primary"
-              type="submit"
-              fullWidth
-              disabled={isLoading}
-              sx={{fontWeight:"bold", transition:"2s" ,fontSize:"20px"}}
-            >
-              Login
-            </Button>
 
-
+          <Button
+            variant="contained"
+            color="primary"
+            type="submit"
+            fullWidth
+            disabled={isLoading}
+            sx={{ fontWeight: "bold", transition: "2s", backgroundColor: "#2196f3", fontSize: "20px", margin: "0px" }}
+          >
+            Login
+          </Button>
+          <p className='-my-4 mx-auto ' >or</p>
+          <p className='mx-auto' > Is not sign up<a className='w-full text-blue-400 ' href={"./sign-up"}>Sign Up</a> ?</p>
         </div>
       </form>
-
     </div>
   )
 }
--- client/src/component/NavBar.jsx
+++ client/src/component/NavBar.jsx
@@ -1,27 +1,85 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useMediaQuery } from 'usehooks-ts'
+import Drawer from '@mui/joy/Drawer';
+import Button from '@mui/joy/Button';
+import LoginIcon from '@mui/icons-material/Login';
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LogoutIcon from '@mui/icons-material/Logout';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
 
 const NavBar = () => {
+
+  const [open, setOpen] = React.useState(false);
+  const { pathname } = useLocation()
+  const matches = useMediaQuery('(min-width: 768px)')
+  
+  const toggleDrawer = (inOpen) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setOpen(inOpen);
+  };
+
   return (
-    <div className=' nav w-screen px-6 py-3'>
+    <div className=' nav w-screen px-6 py-5'>
       <div className='flex justify-between'>
         <div>
           Logo
           ShopApp
         </div>
         <div>
-          <ul className='flex gap-4 ' >
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/cart">cart</Link></li>
-            <li><Link to="/Products">Products</Link></li>
-            <li><Link to="/order">order</Link></li>
-            <li><Link to="/sign-up">register</Link></li>
-            <li><Link to="/sign-in">Login</Link> </li>
-          </ul>
+          {matches ?
+            <>
+              <ul className='flex gap-8 ' >
+                <li className={`duration-200 ${pathname == "/" ? 'border-b-2' : "border-0"}`}  ><Link to="/">  <HomeIcon />  Home</Link></li>
+                <li className={`duration-200 ${pathname == "/Products" ? 'border-b-2' : "border-0"}`}  ><Link to="/Products"> <SearchIcon /> Products</Link></li>
+                <li className={`duration-200 ${pathname == "/cart" ? 'border-b-2' : "border-0"}`} ><Link to="/cart"><ShoppingCartIcon /></Link></li>
+                <li className={`duration-200 ${pathname == "/sign-up" ? 'border-b-2' : "border-0"}`} ><Link to="/sign-up"> <PersonIcon /> register</Link></li>
+                <li className={`duration-200 ${pathname == "/sign-in" ? 'border-b-2' : "border-0"}`} ><LoginIcon /><Link to="/sign-in">Login</Link> </li>
+                {/* <li> <LogoutIcon fontSize='sm' /> <Link to="/sign-in">Logout</Link> </li> */}
+              </ul>
+            </>
+
+            :
+            <>
+
+              <Button variant="outlined" color="neutral" onClick={toggleDrawer(true)}>
+                Open drawer
+              </Button>
+
+              <Drawer
+                open={open}
+                anchor="right"
+                size="lg"
+                color="primary"
+                invertedColors
+                variant="outlined"
+                onClose={toggleDrawer(false)}>
+                <ul className='flex gap-4 my-5 justify-between h-full flex-col '  >
+                  <div className='flex gap-4 flex-col' >
+                    <div className='bg-blue-700 text-white p-5 gap-3 flex ' ><PersonIcon />  <Link to="/sign-up">Login & Sign Up</Link>
+                    </div>
+                    <div className='text-gray-700 flex gap-3 p-3' > <HomeIcon /> <Link to="/">Home</Link></div>
+                    <div className='text-gray-700 flex gap-3 p-3' > <FavoriteIcon /> <Link to="/like">My Watchlist</Link></div>
+                    <div className='text-gray-700 flex gap-3 p-3' > <ShoppingCartIcon /> <Link to="/cart"> My Cart</Link></div>
+                    <div className='text-gray-700 flex gap-3 p-3' ><SearchIcon />  <Link to="/Products">Explore</Link></div>
+                    <div className='text-gray-700 flex gap-3 p-3' > <BookmarkBorderIcon /><Link to="/order">My order</Link></div>
+                  </div>
+                  <button className='rounded-xl bg-blue-600 text-white  border-b-slate-800 p-5 mx-7' >Logout</button>
+                </ul>
+              </Drawer>
+            </>
+          }
+
         </div>
       </div>
 
-    </div>
+    </div >
   )
 }
 
