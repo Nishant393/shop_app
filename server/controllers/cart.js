2a31df24bf52136a69fcc58fbf2bf2ea7a488f16
nikhilchandurkar <nikhilchandurkar24@gamil.com>
2025-01-31 13:06:56 +0530

    "cart modified"

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -1,49 +1,33 @@
-import { ErrorHandler } from "../constant/config.js";
 import { Cart } from "../models/cart.js";
-import { Products } from "../models/product.js";
-import { User } from "../models/user.js";
 
-
-const addToCart = async (req, res) => {
+const addToCart = async (req, res,next) => {
     try {
-        const {userId, productId, quantity } = req.body;
-        // const userId = req.user.id; // Assuming user ID is available from authentication middleware
+        const { userId, productId, quantity = 1 } = req.body;
 
-        let cartItem = await Cart.findOne({ user: userId, product: productId });
-
-        if (cartItem) {
-            cartItem.quantity += quantity || 1;
-            await cartItem.save();
-        } else {
-            cartItem = new Cart({
-                user: userId,
-                product: productId,
-                quantity: quantity || 1,
-            });
-            await cartItem.save();
-        }
+        const cartItem = await Cart.findOneAndUpdate(
+            { user: userId, product: productId }, 
+            { $inc: { quantity } }, 
+            { new: true, upsert: true }
+        );
 
         res.status(200).json({ success: true, message: "Product added to cart", cartItem });
     } catch (error) {
+        console.log(error)
         res.status(500).json({ success: false, message: "Server Error", error: error.message });
     }
 };
 
-
-
-const getCartDetails = async(req,res,next)=>{
+const getCartDetails = async (req, res) => {
     try {
-        const {id} =req.params
-        // const id = req.user
-        const result = await Cart.find({user:id})
-        
-        res.status(200).json({
-            result
-        })
+        const { id } = req.params;
+        const result = await Cart.find({ user: id })
+            .populate({ path: "product", populate: { path: "brand" } });
+
+        res.status(200).json({ success: true, result });
     } catch (error) {
         console.log(error)
+        res.status(500).json({ success: false, message: "Server Error", error: error.message });
     }
-}
+};
 
-
-export{addToCart, getCartDetails }
+export { addToCart, getCartDetails };
--- server/models/cart.js
+++ server/models/cart.js
@@ -1,20 +1,15 @@
 import mongoose, { Schema, model } from "mongoose";
 
-const cartSchema = new Schema({
-   user: {
-       type: Schema.Types.ObjectId,
-       ref: "User",
-       required: true,
-   },
-   product: {
-       type: Schema.Types.ObjectId,
-       ref: "Product",
-       required: true
-   },
-   quantity: {
-       type: Number,
-       default: 1,
-   }
-}, { versionKey: false, timestamps: true });
+const cartSchema = new Schema(
+    {
+        user: { type: Schema.Types.ObjectId, 
+            ref: "User", required: true },
+        product: { type: Schema.Types.ObjectId,
+             ref: "Product", required: true },
+        quantity: { type: Number, default: 1, min: 1 }
+    }, 
+    { versionKey: false, 
+        timestamps: true }
+);
 
 export const Cart = mongoose.models.Cart || model("Cart", cartSchema);
