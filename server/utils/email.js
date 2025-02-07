472bf32f9c8528fbe805157c23c0ea81674a3e7f
nikhilchandurkar <nikhilchandurkar24@gamil.com>
2025-02-07 15:59:21 +0530

    "merge"

--- server/utils/email.js
+++ server/utils/email.js
@@ -4,22 +4,21 @@ import nodemailer from "nodemailer"
 const emailuser =process.env.EMAIL_USER
 const emailAppPassword = process.env.EMAIL_PASS
 
-const transporter = nodemailer.createTransport(
-  {
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // `true` for port 465, `false` for 587
-  auth: {
-    user: emailuser,
-    pass: emailAppPassword,
-  },
-}
-);
 
 const sendMail = async (receiverEmail, subject, body) => {
   try {
     console.log("auth value", transporter.auth)
-    await transporter.sendMail({
+    await nodemailer.createTransport(
+      {
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, 
+      auth: {
+        user: emailuser,
+        pass: emailAppPassword,
+      },
+    }
+    ).sendMail({
       from: process.env.EMAIL_USER,
       to: receiverEmail,
       subject: subject,
