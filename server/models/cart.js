2e5920b3812f050bbf1d4fc408ac503a0a3a07ff
nikhilchandurkar <nikhilchandurkar24@gamil.com>
2025-02-14 19:42:47 +0530

    "fix in cart"

--- server/models/cart.js
+++ server/models/cart.js
@@ -8,7 +8,7 @@ const CartSchema = new mongoose.Schema({
     },
     product: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
+      ref: 'Products',
       required: true
     },
     quantity: {
